5f31bf169c3dc6e60470b0dd7e4798f354872817
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-16 22:42:51 +0530

    Add more URLs and views

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world, You are at blog's index")
+
+def detail(request):
+    return HttpResponse("Your are viewing post detail page")
