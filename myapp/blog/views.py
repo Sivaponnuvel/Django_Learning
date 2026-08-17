63c2238749f92b744210610ab3f40c5ffe37ff47
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-17 23:22:41 +0530

    Add HTML templates for blog views

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,10 +5,10 @@ from django.urls import reverse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world, You are at blog's index")
+    return render(request,"blog/index.html")
 
 def detail(request, post_id):
-    return HttpResponse(f"Your are viewing post detail page. And ID is {post_id}")
+    return render(request, "blog/detail.html")
 
 def old_url_redirect(request):
     return redirect(reverse("blog:new_page_url"))
