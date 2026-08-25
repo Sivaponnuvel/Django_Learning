3eae0f11d316bd4acd6cacbb4706ed2b4dd9abc8
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-25 22:24:28 +0530

    Add for tag for blog posts

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -6,7 +6,13 @@ from django.urls import reverse
 
 def index(request):
     blog_title = "Latest Posts"
-    return render(request,"blog/index.html", {'blog_title': blog_title})
+    posts = [
+        {'title': 'Post 1', 'content': 'Content of Post 1'},
+        {'title': 'Post 2', 'content': 'Content of Post 2'},
+        {'title': 'Post 3', 'content': 'Content of Post 3'},
+        {'title': 'Post 4', 'content': 'Content of Post 4'},
+    ]
+    return render(request,"blog/index.html", {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
     return render(request, "blog/detail.html")
