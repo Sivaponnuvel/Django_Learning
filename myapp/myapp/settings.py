5a936dfb7a128f7e755fa46e8c466ee5d28be42c
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-26 21:58:46 +0530

    Build post detail page and add logging

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -132,3 +132,17 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
