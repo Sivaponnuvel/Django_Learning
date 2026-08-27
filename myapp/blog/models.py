c99c69e31a1b1d852d6ec01d3d068b44eb2b69d2
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-27 15:13:36 +0530

    Create Post model with title, content, and image fields

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
