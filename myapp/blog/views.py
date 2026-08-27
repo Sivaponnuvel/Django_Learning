f23f6be5a546999b960e640369234e528e846b6b
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-27 22:54:38 +0530

    Fetch posts from database and render them dynamically

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
+from .models import Post
 
 # Create your views here.
-posts = [
-    {'id': 1, 'title': 'Post 1', 'content': 'Content of Post 1'},
-    {'id': 2, 'title': 'Post 2', 'content': 'Content of Post 2'},
-    {'id': 3, 'title': 'Post 3', 'content': 'Content of Post 3'},
-    {'id': 4, 'title': 'Post 4', 'content': 'Content of Post 4'},
-]
+# static demo data
+# posts = [
+#     {'id': 1, 'title': 'Post 1', 'content': 'Content of Post 1'},
+#     {'id': 2, 'title': 'Post 2', 'content': 'Content of Post 2'},
+#     {'id': 3, 'title': 'Post 3', 'content': 'Content of Post 3'},
+#     {'id': 4, 'title': 'Post 4', 'content': 'Content of Post 4'},
+# ]
 
 def index(request):
     blog_title = "Latest Posts"
+    # getting data from post model
+    posts = Post.objects.all()
     return render(request,"blog/index.html", {'blog_title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
