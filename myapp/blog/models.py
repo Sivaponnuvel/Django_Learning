b225d4e748d815eeb329a5fa8f09a533a0a9547a
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-08-29 19:38:12 +0530

    Add many-to-one relationship between posts and categories

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 from django.utils.text import slugify
 
-# Create your models here.
+# category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     img_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -17,9 +24,3 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-# category
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
