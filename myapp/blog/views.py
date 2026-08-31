c4a85e2a9322312e6b77850347e602ffc656fffc
Sivaponnuvel <sivaponnuvel7@gmail.com>
2026-09-01 00:47:18 +0530

    Add pagination for blog posts

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 import logging
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 # static demo data
@@ -17,8 +18,14 @@ from django.http import Http404
 def index(request):
     blog_title = "Latest Posts"
     # getting data from post model
-    posts = Post.objects.all()
-    return render(request,"blog/index.html", {'blog_title': blog_title, 'posts': posts})
+    all_posts = Post.objects.all()
+
+    # paginate
+    paginator = Paginator(all_posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request,"blog/index.html", {'blog_title': blog_title, 'page_obj': page_obj})
 
 def detail(request, slug):
     # static data
